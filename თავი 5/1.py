c19ca0d3b171762d50ed29297c9abcd484ebf253
Giorgi-Babuadze <justyrbxrru@gmail.com>
2025-02-27 22:06:53 +0400

    Delete თავი 5 directory

--- თავი 5/1.py
+++
@@ -1,4 +0,0 @@
-def calculate(a, b):
-    return a * b
-print(calculate(5, 7))
-# Output: 35
--- თავი 5/2.py
+++
@@ -1,4 +0,0 @@
-def hello(name):
-    print('გამარჯობა ' + name + "!")
-hello('გიორგი')
-# Output: გამარჯობა გიორგი!
