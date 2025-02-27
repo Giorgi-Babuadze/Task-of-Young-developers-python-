9a258a045ca5b5d1dbafe19e8c4c36cc36638d34
Giorgi-Babuadze <justyrbxrru@gmail.com>
2025-02-27 22:06:25 +0400

    Delete თავი 2 directory

--- თავი 2/1.py
+++
@@ -1,5 +0,0 @@
-name = 'Giorgi'
-age = 14
-fav_color = 'Blue'
-print(name, age, fav_color)
-# Output: Giorgi 14 Blue
--- თავი 2/2.py
+++
@@ -1,4 +0,0 @@
-fav_fruits = ['Apple', 'Banana', 'Orange']
-fav_fruits.append('Kiwi')
-print(fav_fruits)
-# Output: ['Apple', 'Banana', 'Orange', 'Kiwi']
